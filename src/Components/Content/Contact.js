d05f6ef672cc60d0b11d9a286d3e5918911e3fce
Tali <taliitacamargo@icloud.com>
2022-02-20 18:44:14 -0500

    more styling

--- src/Components/Content/Contact.js
+++ src/Components/Content/Contact.js
@@ -74,22 +74,22 @@ const Contact = () =>  {
                 <nav className="nav" id="contact-info">
                     <ul className="links">
                         <li>
-                            <a style={{ "fontSize": "45px" }} href="mailto:taliitacamargo@icloud.com"><FaEnvelopeOpen className="grow" /></a>
+                            <a className="grow" href="mailto:taliitacamargo@icloud.com"><FaEnvelopeOpen className="grow" /></a>
                             <small>Email</small>
 
                         </li>
                         <li>
-                            <a style={{ "fontSize": "45px" }} href={resume}><FaUserTie className="grow" /></a>
+                            <a className="grow"  href={resume}><FaUserTie className="grow" /></a>
                             <small>Resume</small>
 
                         </li>
                         <li>
-                            <a style={{ "fontSize": "45px" }} href="https://www.linkedin.com/in/talita-camargo-03a40a52/"><FaLinkedin className="grow" /></a>
+                            <a className="grow" href="https://www.linkedin.com/in/talita-camargo-03a40a52/"><FaLinkedin className="grow" /></a>
                             <small>LinkedIn</small>
 
                         </li>
                         <li>
-                            <a style={{ "fontSize": "45px" }} href="https://github.com/taliitacamargo"><FaGithubSquare className="grow" /></a>
+                            <a className="grow" href="https://github.com/taliitacamargo"><FaGithubSquare className="grow" /></a>
 
                             <small>GitHub</small>
 
