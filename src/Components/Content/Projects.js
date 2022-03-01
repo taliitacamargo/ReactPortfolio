078d51a26eb934fd079eeaa870333b9493281523
Tali <taliitacamargo@icloud.com>
2022-03-01 14:25:59 -0500

    sizing

--- src/Components/Content/Projects.js
+++ src/Components/Content/Projects.js
@@ -8,10 +8,10 @@ export default function Projects() {
         <h1>My Projects</h1>
         <div className = "prj-container project-container">
             
-            <a className= "projects" href = "https://fasttrackpenn.herokuapp.com/"><h3>FastTrack</h3>
+            <a className= "projects" href = "https://fasttrackpenn.herokuapp.com/"><small>FastTrack</small>
             <img className = "project-img" src ={fastTrack } alt="fastrack"/> </a>
             
-            <a className= "projects" href = "https://toriskyt.github.io/project-one/"><h3>TixShare</h3>
+            <a className= "projects" href = "https://toriskyt.github.io/project-one/"><small>TixShare</small>
             <img className = "project-img" src ={tixShare} alt="fastrack"/> </a>
       
         </div>
