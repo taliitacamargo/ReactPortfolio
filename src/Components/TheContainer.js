c6ad159546e3b46e42b17172c056dc21ac80ddf6
Tali <taliitacamargo@icloud.com>
2022-03-01 14:06:12 -0500

    removing video styling from css adding to react file

--- src/Components/TheContainer.js
+++ src/Components/TheContainer.js
@@ -30,7 +30,17 @@ export default function TheContainer() {
         <div>
           
 
-                <video className='videoTag' autoPlay loop muted>
+                <video className='videoTag' autoPlay loop muted style = {{
+              
+                    position: "fixed",
+                    width: "100%",
+                    left: "50%",
+                    top: "50%",
+                    height: "100%",
+                    objectFit: "cover",
+                    transform: "translate(-50%, -50%) ",
+                    zIndex: "-1", 
+                }}>
                     <source src={backgroundVideo} type='video/mp4' />
                 </video>
 
