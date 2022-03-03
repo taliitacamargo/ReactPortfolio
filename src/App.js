3e95b007a019ac5ddf4b7299630dfac6b666b36a
Tali <taliitacamargo@icloud.com>
2022-03-03 15:02:10 -0500

    changing projects file

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import TheContainer from './Components/TheContainer';
 import '../src/App.css';
 import Footer from './Components/Footer'
 import backgroundVideo from './Components/assets/particles.mp4'
+
 // import backgroundVideo from '../src/Components/Content/knight.mp4';
 
 function App() {
     return (
          <div className =  "app.js">
 <TheContainer/>
-
  </div>
     )
 }
--- src/Components/Content/Projects.js
+++ src/Components/Content/Projects.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import fastTrack from '../assets/FastTrack.jpg';
-import tixShare from '../assets/TixShare.jpeg'
+import tixShare from '../assets/TixShare.jpeg';
+import cardGraphix from '../assets/CardGraphix.jpg';
+import budgetTracker from '../assets/BudgetTracker.jpg';
+import fitnessTracker from '../assets/FitnessTracker.jpg';
+import quizChallenge from '../assets/QuizChallenge.jpg'
 
 export default function Projects() {
     return(
@@ -8,12 +12,23 @@ export default function Projects() {
         <h1>My Projects</h1>
         <div className = "prj-container project-container">
             
-            <a className= "projects" href = "https://fasttrackpenn.herokuapp.com/"><small>FastTrack</small>
-            <img className = "project-img" src ={fastTrack } alt="fastrack"/> </a>
+            <a className= "projects" href = "https://fasttrackpenn.herokuapp.com/"><small className = "prj-name">FastTrack</small>
+            <img className = "project-img block1" src ={fastTrack } alt="fastrack"/> </a>
             
-            <a className= "projects" href = "https://toriskyt.github.io/project-one/"><small>TixShare</small>
-            <img className = "project-img" src ={tixShare} alt="fastrack"/> </a>
+            <a className= "projects" href = "https://toriskyt.github.io/project-one/"><small className = "prj-name">TixShare</small>
+            <img className = "project-img block1" src ={tixShare} alt="fastrack"/> </a>
+
+            <a className= "projects" href = "https://card-graphix.herokuapp.com/"><small className = "prj-name">CardGraphix</small>
+            <img className = "project-img block2 " src ={cardGraphix} alt="cardgraphix"/> </a>
+
+            <a className= "projects" href = "https://powerful-plains-32252.herokuapp.com"><small className = "prj-name">BudgetTracker</small>
+            <img className = "project-img block2" src ={budgetTracker} alt="budgettracker"/> </a>
       
+            <a className= "projects" href = "https://mysterious-tundra-72660.herokuapp.com"><small className = "prj-name">FitnessTracker</small>
+            <img className = "project-img block2" src ={fitnessTracker} alt="fitnesstracker"/> </a>
+
+            <a className= "projects" href = "https://taliitacamargo.github.io/quiz-project/"><small className = "prj-name">QuizChallenge</small>
+            <img className = "project-img block2" src ={quizChallenge} alt="quizchallenge"/> </a>
         </div>
         
     </div>
